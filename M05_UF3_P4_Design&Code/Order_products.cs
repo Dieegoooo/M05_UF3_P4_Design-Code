64daeeb5c309892ba6fb3b7ce6dc4c1554c27bd7
Dieegoooo <115780715+Dieegoooo@users.noreply.github.com>
2023-04-30 18:40:24 +0200

    4.5

--- M05_UF3_P4_Design&Code/Order_products.cs
+++ M05_UF3_P4_Design&Code/Order_products.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace M05_UF3_P4_Design_Code
 {
-    internal class Order_products
+    public class Order_products
     {
         public string product;
         public int order_number;
