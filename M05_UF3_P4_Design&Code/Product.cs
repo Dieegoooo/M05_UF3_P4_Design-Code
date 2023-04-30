22cd5e27b511ee4ab22efca96e52d15386597f03
Dieegoooo <115780715+Dieegoooo@users.noreply.github.com>
2023-04-30 18:36:27 +0200

    commit 4.1

--- M05_UF3_P4_Design&Code/Product.cs
+++ M05_UF3_P4_Design&Code/Product.cs
@@ -6,22 +6,22 @@ namespace M05_UF3_P4_Design_Code
 {
     public class Product
     {
-            public int id;
-            public string name;
-            public string brand;
-            public string imgUrl;
-            public string description;
-            public int base_price;
-            public List<string> valuation;
-            public List<string> labels;
-            public int discount;
-            public List<string> category;
-            public List<string> subcategory;
+        public string reference;
+        public string name;
+        public string brand;
+        public string imgUrl;
+        public string description;
+        public float base_price;
+        public List<string> valuation;
+        public List<string> labels;
+        public float discount;
+        public List<string> category;
+        public List<string> subcategory;
 
         public class SpecializedProduct : Product
         {
             public List<string> subsubcategory;
         }
     }
-    
+
 }
