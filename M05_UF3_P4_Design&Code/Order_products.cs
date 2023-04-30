e1eade79cd5b574ef62bce325b5562dd3f1a6f52
Dieegoooo <115780715+Dieegoooo@users.noreply.github.com>
2023-04-30 18:40:57 +0200

    4.7

--- M05_UF3_P4_Design&Code/Order_products.cs
+++ M05_UF3_P4_Design&Code/Order_products.cs
@@ -7,7 +7,7 @@ namespace M05_UF3_P4_Design_Code
     public class Order_products
     {
         public string product;
-        public int order_number; 
+        public int order_number;
         public int quantity;
     }
 }
